450f975d258c6ecb8e81bf175b36324c025fb377
alanyuan97 <36449601+alanyuan97@users.noreply.github.com>
2020-02-06 15:19:17 +0000

    Week 3 work

---
+++ Week3/lab3/.metadata/.plugins/org.eclipse.core.resources/.history/bb/1068c7a07c43001a163ebf1f75e3ea86
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+build-410347057:
+	@$(MAKE) --no-print-directory -Onone -f subdir_rules.mk build-410347057-inproc
+
+build-410347057-inproc: ../dsp_bios_.tcf
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: TConf'
+	"C:/ti/bios_5_42_02_10/xdctools/tconf" -b -Dconfig.importPath="C:/ti/bios_5_42_02_10/packages;" "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+dsp_bios_cfg.cmd: build-410347057 ../dsp_bios_.tcf
+dsp_bios_cfg.s??: build-410347057
+dsp_bios_cfg_c.c: build-410347057
+dsp_bios_cfg.h: build-410347057
+dsp_bios_cfg.h??: build-410347057
+dsp_bios_.cdb: build-410347057
+
+dsp_bios_cfg.obj: ./dsp_bios_cfg.s?? $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv8/tools/compiler/c6000_7.4.23/bin/cl6x" -mv6700 --abi=coffabi -g --include_path="H:/RTDSPlab/lab3/Sine_wave_generator" --include_path="C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include" --include_path="H:/RTDSPlab/lab3/Sine_wave_generator/Debug" --include_path="C:/ti/bios_5_42_02_10/packages/ti/bios/include" --include_path="C:/ti/bios_5_42_02_10/packages/ti/rtdx/include/c6000" --include_path="C:/ti/ccsv8/C6000/dsk6713/include" --include_path="C:/ti/ccsv8/C6000/csl/include" --define=c6713 --define="_DEBUG" --define="CHIP_6713" --diag_wrap=off --display_error_number --diag_warning=225 --preproc_with_compile --preproc_dependency="dsp_bios_cfg.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+dsp_bios_cfg_c.obj: ./dsp_bios_cfg_c.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv8/tools/compiler/c6000_7.4.23/bin/cl6x" -mv6700 --abi=coffabi -g --include_path="H:/RTDSPlab/lab3/Sine_wave_generator" --include_path="C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include" --include_path="H:/RTDSPlab/lab3/Sine_wave_generator/Debug" --include_path="C:/ti/bios_5_42_02_10/packages/ti/bios/include" --include_path="C:/ti/bios_5_42_02_10/packages/ti/rtdx/include/c6000" --include_path="C:/ti/ccsv8/C6000/dsk6713/include" --include_path="C:/ti/ccsv8/C6000/csl/include" --define=c6713 --define="_DEBUG" --define="CHIP_6713" --diag_wrap=off --display_error_number --diag_warning=225 --preproc_with_compile --preproc_dependency="dsp_bios_cfg_c.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+intio.obj: ../intio.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv8/tools/compiler/c6000_7.4.23/bin/cl6x" -mv6700 --abi=coffabi -g --include_path="H:/RTDSPlab/lab3/Sine_wave_generator" --include_path="C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include" --include_path="H:/RTDSPlab/lab3/Sine_wave_generator/Debug" --include_path="C:/ti/bios_5_42_02_10/packages/ti/bios/include" --include_path="C:/ti/bios_5_42_02_10/packages/ti/rtdx/include/c6000" --include_path="C:/ti/ccsv8/C6000/dsk6713/include" --include_path="C:/ti/ccsv8/C6000/csl/include" --define=c6713 --define="_DEBUG" --define="CHIP_6713" --diag_wrap=off --display_error_number --diag_warning=225 --preproc_with_compile --preproc_dependency="intio.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
---
+++ Week3/lab3/Sine_wave_generator/Debug/intio.d
@@ -0,0 +1,121 @@
+# FIXED
+
+intio.obj: ../intio.c
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stdlib.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/linkage.h
+intio.obj: H:/RTDSPlab/lab3/Sine_wave_generator/Debug/dsp_bios_cfg.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/std.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/tistdtypes.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/hst.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/pip.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stddef.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/fxn.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/sts.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/swi.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/hwi.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/tsk.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stdarg.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stddef.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/knl.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/atm.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/que.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/mem.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stddef.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stdarg.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/prd.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/sem.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/obj.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stddef.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/sys.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stdarg.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/trg.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stddef.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/_hook.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/log.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/_log.h
+intio.obj: C:/ti/bios_5_42_02_10/packages/ti/bios/include/trc.h
+intio.obj: C:/ti/ccsv8/C6000/dsk6713/include/dsk6713.h
+intio.obj: C:/ti/ccsv8/C6000/csl/include/csl.h
+intio.obj: C:/ti/ccsv8/C6000/csl/include/csl_chip.h
+intio.obj: C:/ti/ccsv8/C6000/csl/include/csl_stdinc.h
+intio.obj: C:/ti/ccsv8/C6000/csl/include/csl_stdinchal.h
+intio.obj: C:/ti/ccsv8/C6000/csl/include/csl_chiphal.h
+intio.obj: C:/ti/ccsv8/C6000/csl/include/csl_emifhal.h
+intio.obj: C:/ti/ccsv8/C6000/csl/include/csl_chip.h
+intio.obj: C:/ti/ccsv8/C6000/csl/include/csl_irq.h
+intio.obj: C:/ti/ccsv8/C6000/csl/include/csl_irqhal.h
+intio.obj: C:/ti/ccsv8/C6000/csl/include/csl_timer.h
+intio.obj: C:/ti/ccsv8/C6000/csl/include/csl_timerhal.h
+intio.obj: C:/ti/ccsv8/C6000/dsk6713/include/dsk6713_aic23.h
+intio.obj: C:/ti/ccsv8/C6000/csl/include/csl_mcbsp.h
+intio.obj: C:/ti/ccsv8/C6000/csl/include/csl_mcbsphal.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/math.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/float.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/access.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/elfnames.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/mathf.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/unaccess.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/mathl.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/unaccess.h
+intio.obj: C:/ti/ccsv8/C6000/dsk6713/include/helper_functions_ISR.h
+intio.obj: C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/math.h
+
+../intio.c: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stdlib.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/linkage.h: 
+H:/RTDSPlab/lab3/Sine_wave_generator/Debug/dsp_bios_cfg.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/std.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/tistdtypes.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/hst.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/pip.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stddef.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/fxn.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/sts.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/swi.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/hwi.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/tsk.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stdarg.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stddef.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/knl.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/atm.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/que.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/mem.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stddef.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stdarg.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/prd.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/sem.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/obj.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stddef.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/sys.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stdarg.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/trg.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/stddef.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/_hook.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/log.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/_log.h: 
+C:/ti/bios_5_42_02_10/packages/ti/bios/include/trc.h: 
+C:/ti/ccsv8/C6000/dsk6713/include/dsk6713.h: 
+C:/ti/ccsv8/C6000/csl/include/csl.h: 
+C:/ti/ccsv8/C6000/csl/include/csl_chip.h: 
+C:/ti/ccsv8/C6000/csl/include/csl_stdinc.h: 
+C:/ti/ccsv8/C6000/csl/include/csl_stdinchal.h: 
+C:/ti/ccsv8/C6000/csl/include/csl_chiphal.h: 
+C:/ti/ccsv8/C6000/csl/include/csl_emifhal.h: 
+C:/ti/ccsv8/C6000/csl/include/csl_chip.h: 
+C:/ti/ccsv8/C6000/csl/include/csl_irq.h: 
+C:/ti/ccsv8/C6000/csl/include/csl_irqhal.h: 
+C:/ti/ccsv8/C6000/csl/include/csl_timer.h: 
+C:/ti/ccsv8/C6000/csl/include/csl_timerhal.h: 
+C:/ti/ccsv8/C6000/dsk6713/include/dsk6713_aic23.h: 
+C:/ti/ccsv8/C6000/csl/include/csl_mcbsp.h: 
+C:/ti/ccsv8/C6000/csl/include/csl_mcbsphal.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/math.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/float.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/access.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/elfnames.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/mathf.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/unaccess.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/mathl.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/unaccess.h: 
+C:/ti/ccsv8/C6000/dsk6713/include/helper_functions_ISR.h: 
+C:/ti/ccsv8/tools/compiler/c6000_7.4.23/include/math.h: 
