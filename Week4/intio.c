a6e09cda4a5e3fc79332eafb87208d06e96a1c39
alanyuan97 <36449601+alanyuan97@users.noreply.github.com>
2020-02-18 18:58:01 +0000

    Added cir_fir

--- Week4/intio.c
+++ Week4/intio.c
@@ -1,6 +1,6 @@
 /*************************************************************************************
 			       DEPARTMENT OF ELECTRICAL AND ELECTRONIC ENGINEERING
-					   		     IMPERIAL COLLEGE LONDON 
+					   		     IMPERIAL COLLEGE LONDON
 
  				      EE 3.19: Real Time Digital Signal Processing
 					       Dr Paul Mitcheson and Daniel Harvey
@@ -9,45 +9,45 @@
 
  				            ********* I N T I O. C **********
 
-  Demonstrates inputing and outputing data from the DSK's audio port using interrupts. 
+  Demonstrates inputing and outputing data from the DSK's audio port using interrupts.
 
  *************************************************************************************
  				Updated for use on 6713 DSK by Danny Harvey: May-Aug 2006
 				Updated for CCS V4 Sept 10
  ************************************************************************************/
 /*
- *	You should modify the code so that interrupts are used to service the 
+ *	You should modify the code so that interrupts are used to service the
  *  audio port.
  */
 /**************************** Pre-processor statements ******************************/
 
 #include <stdlib.h>
-//  Included so program can make use of DSP/BIOS configuration tool.  
+//  Included so program can make use of DSP/BIOS configuration tool.
 #include "dsp_bios_cfg.h"
 
-/* The file dsk6713.h must be included in every program that uses the BSL.  This 
-   example also includes dsk6713_aic23.h because it uses the 
+/* The file dsk6713.h must be included in every program that uses the BSL.  This
+   example also includes dsk6713_aic23.h because it uses the
    AIC23 codec module (audio interface). */
 #include "dsk6713.h"
 #include "dsk6713_aic23.h"
 
 // math library (trig functions)
 #include <math.h>
-
+#include <fir_cof.txt>
 // Some functions to help with writing/reading the audio ports when using interrupts.
 #include <helper_functions_ISR.h>
 
 
 #define PI 3.141592653589793
 #define SINE_TABLE_SIZE 256
-#define CIRBUFFER 249
+#define filter_size 249
 /******************************* Global declarations ********************************/
 
-/* Audio port configuration settings: these values set registers in the AIC23 audio 
+/* Audio port configuration settings: these values set registers in the AIC23 audio
    interface to configure it. See TI doc SLWS106D 3-3 to 3-10 for more info. */
 DSK6713_AIC23_Config Config = { \
 			 /**********************************************************************/
-			 /*   REGISTER	            FUNCTION			      SETTINGS         */ 
+			 /*   REGISTER	            FUNCTION			      SETTINGS         */
 			 /**********************************************************************/\
     0x0017,  /* 0 LEFTINVOL  Left line input channel volume  0dB                   */\
     0x0017,  /* 1 RIGHTINVOL Right line input channel volume 0dB                   */\
@@ -64,67 +64,62 @@ DSK6713_AIC23_Config Config = { \
 
 int sample;
 float table[SINE_TABLE_SIZE];
-
-double CIRBUFF[CIRBUFFER]= {0}; // Initialize array2 0s
-double *cir_ptr = CIRBUFF; // Start pointer of cir_array
-double *cir_ptr_end = CIRBUFF[CIRBUFFER-1]; // End pointer of cir_array
-double *coef_ptr = b; // Start of coef array 
-double *coef_end = b[CIRBUFFER-1]; // End of coef array
-int offset=0;
+double input[filter_size];
 int index=0;
-
-// Codec handle:- a variable used to identify audio interface  
+int offset=0;
+// Codec handle:- a variable used to identify audio interface
 DSK6713_AIC23_CodecHandle H_Codec;
 
  /******************************* Function prototypes ********************************/
-void init_hardware(void);     
-void init_HWI(void);                   
+void init_hardware(void);
+void init_HWI(void);
 void InterupptSR();
-void sine_init();
+void test_cir();
 /********************************** Main routine ************************************/
-void main(){      
+void main(){
 
- 
+
 	// initialize board and the audio port
   init_hardware();
-	
+
   /* initialize hardware interrupts */
   init_HWI();
-  sine_init();
-  	 		
-  /* loop indefinitely, waiting for interrupts */  					
-  while(1) 
+//  sine_init();
+
+
+  /* loop indefinitely, waiting for interrupts */
+  while(1)
   {};
-  
+
 }
-        
-/********************************** init_hardware() **********************************/  
+
+/********************************** init_hardware() **********************************/
 void init_hardware()
 {
-    // Initialize the board support library, must be called first 
+    // Initialize the board support library, must be called first
     DSK6713_init();
-    
-    // Start the AIC23 codec using the settings defined above in config 
+
+    // Start the AIC23 codec using the settings defined above in config
     H_Codec = DSK6713_AIC23_openCodec(0, &Config);
 
-	/* Function below sets the number of bits in word used by MSBSP (serial port) for 
-	receives from AIC23 (audio port). We are using a 32 bit packet containing two 
+	/* Function below sets the number of bits in word used by MSBSP (serial port) for
+	receives from AIC23 (audio port). We are using a 32 bit packet containing two
 	16 bit numbers hence 32BIT is set for  receive */
-	MCBSP_FSETS(RCR1, RWDLEN1, 32BIT);	
+	MCBSP_FSETS(RCR1, RWDLEN1, 32BIT);
 
-	/* Configures interrupt to activate on each consecutive available 32 bits 
-	from Audio port hence an interrupt is generated for each L & R sample pair */	
+	/* Configures interrupt to activate on each consecutive available 32 bits
+	from Audio port hence an interrupt is generated for each L & R sample pair */
 	MCBSP_FSETS(SPCR1, RINTM, FRM);
 
-	/* These commands do the same thing as above but applied to data transfers to  
+	/* These commands do the same thing as above but applied to data transfers to
 	the audio port */
-	MCBSP_FSETS(XCR1, XWDLEN1, 32BIT);	
-	MCBSP_FSETS(SPCR1, XINTM, FRM);	
-	
+	MCBSP_FSETS(XCR1, XWDLEN1, 32BIT);
+	MCBSP_FSETS(SPCR1, XINTM, FRM);
+
 
 }
 
-/********************************** init_HWI() **************************************/  
+/********************************** init_HWI() **************************************/
 void init_HWI(void)
 {
 	IRQ_globalDisable();			// Globally disables interrupts
@@ -133,23 +128,38 @@ void init_HWI(void)
 	IRQ_enable(IRQ_EVT_RINT1);		// Enables the event
 	IRQ_globalEnable();				// Globally enables interrupts
 
-} 
+}
 
-/******************** WRITE YOUR INTERRUPT SERVICE ROUTINE HERE***********************/  
-void cir_fir(){
-    double *buf_val = cir_ptr + offset;
-    *buf_val = mono_read_16Bit();
-    double output = 0;
-    while(buf_val>=cir_ptr){
-        output+= (*buf_val--) * (*coef_ptr++);
-    }
-    buf_val = cir_ptr + CIRBUFFER -1;
+/******************** WRITE YOUR INTERRUPT SERVICE ROUTINE HERE***********************/
+void InteruptSR(){
+    int i;
+    int j;
 
-    while(coef_ptr<cir_ptr_end){
-        output+= (*buf_val--) * (*coef_ptr++);
+
+    input[0]=mono_read_16Bit();//read a new sample and place it into buffer
+
+    double temp=0;// initialise the temporary variable as a accumulator
+    for(j=0;j<filter_size;j++){ // loop through all the elements needed to compute convolution
+      temp+=input[j]*b[j]; // compute element-wise multiplication for convolution
     }
-    if (++offset>=CIRBUFFER){
-        offset = 0;
+    mono_write_16Bit(temp);//write the convolution result to codec
+
+    for (i=filter_size-1;i>0;i--){
+      input[i]=input[i-1]; // shift the buffer by one element in order to make a new space for next incoming input
     }
+}
+
+void test_cir(){
+    input[offset] = mono_read_16Bit();
+    int i;
+    float output = 0;
+    for(i = 0;i<filter_size-offset;i++){
+        output+=input[offset+i]*b[i];
+    }
+    for(i = filter_size-offset;i<filter_size;i++){
+        output+=input[offset+i-filter_size]*b[i];
+    }
+    offset++;
+    if (offset==filter_size){offset=0;}
     mono_write_16Bit(output);
 }
